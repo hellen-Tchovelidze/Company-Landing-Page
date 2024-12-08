10479b04cd5b6e2c332ff78fb204c9906baaa402
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-12-08 12:38:12 +0400

    feat(sereept_logics) : make Javascript

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 let insight_container = document.getElementById("insight_container");
 let browseall = document.getElementById("browseall");
 
+
+
+let CreatBtn = document.querySelector(".sec_creative_btn")
+let Creatp = document.getElementById("Crap")
+let isVisible = false;
+CreatBtn.addEventListener("click",  async (event) => {
+    event.preventDefault
+
+    if (isVisible) {
+        Creatp.innerHTML=""
+        isVisible = false
+        CreatBtn.textContent = "Explore"
+    } else {
+        try {
+            let box = await fetch( `https://fakestoreapi.com/products/1`)
+            let result = await box.json()
+        
+           
+                let p = document.createElement("p")
+                p.textContent = result.description
+                Creatp.appendChild(p)
+                Creatp.classList.toggle("Fechtex")
+          isVisible= true
+            CreatBtn.textContent = "up"
+            
+           } catch (error) {
+          console.log("error");
+          
+           }
+    }
+  
+})
+
+
+
 if (localStorage.getItem("newdivs") === "true") {
   insight_container.classList.add("visible");
 }
@@ -14,3 +49,5 @@ browseall.addEventListener("click", () => {
     localStorage.setItem("newdivs", "true");
   }
 });
+
+
