3473114745aa83b2dce53e444fd2cd8dc9d32389
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-12-12 20:53:52 +0400

    feat(make js_script):created js_ logics
    creating aboutbtn click logic

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 let insight_container = document.getElementById("insight_container");
 let browseall = document.getElementById("browseall");
+<<<<<<< Updated upstream
+=======
+let subscribe_button = document.getElementById("subscribe_button");
+let email = document.getElementById("email");
+let move_left = document.getElementById("move_left");
+let move_right = document.getElementById("move_right");
+let review_paragraph = document.getElementById("review_paragraph");
+let customer_name = document.getElementById("customer_name");
+let microsoft_span = document.getElementById("microsoft_span");
+let aboutBtn = document.querySelector(".sec_about_btn")
+let ulAdd = document.getElementById("ul_add")
+let isAboutVisible = false;
+
+
+// ეს ღილაკი მუშაობს რომ api და წამოიღოს 3 id ლისთისთვის 
+aboutBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  if (isAboutVisible) {
+      ulAdd.innerHTML = "";
+       isAboutVisible  = false;
+      aboutBtn.textContent = "Explore";
+  } else {
+      try {
+          let response = await fetch(`https://fakestoreapi.com/products`);
+          let result = await response.json();
+
+          result.slice(0, 3).forEach(item => {
+              let li = document.createElement("li");
+              li.textContent = item.title;
+              ulAdd.appendChild(li);
+          });
+
+          ulAdd.classList.toggle("li_js");
+          isAboutVisible = true;
+          aboutBtn.textContent = "show less";
+      } catch (error) {
+          console.error("Error fetching data:", error);
+      }
+  }
+});
+
+
+>>>>>>> Stashed changes
 
 
 
