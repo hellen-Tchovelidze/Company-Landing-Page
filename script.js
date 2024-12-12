1ea4ab6579ec4cca1faf7aba13e0c038a2c8db91
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-12-12 23:17:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,76 +8,69 @@ let move_right = document.getElementById("move_right");
 let review_paragraph = document.getElementById("review_paragraph");
 let customer_name = document.getElementById("customer_name");
 let microsoft_span = document.getElementById("microsoft_span");
-let aboutBtn = document.querySelector(".sec_about_btn")
-let ulAdd = document.getElementById("ul_add")
-let CreatBtn = document.querySelector(".sec_creative_btn")
-let Creatp = document.getElementById("Crap")
-let burgerIcon = document.getElementById("burgerIcon")
-let burgerMenu = document.getElementById("burger_menu")
+let aboutBtn = document.querySelector(".sec_about_btn");
+let ulAdd = document.getElementById("ul_add");
+let CreatBtn = document.querySelector(".sec_creative_btn");
+let Creatp = document.getElementById("Crap");
+let burgerIcon = document.getElementById("burgerIcon");
+let burgerMenu = document.getElementById("burger_menu");
 let isVisible = false;
 let isAboutVisible = false;
-CreatBtn.addEventListener("click",  async (event) => {
-    event.preventDefault()
+CreatBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
 
-    if (isVisible) {
-        Creatp.innerHTML=""
-        isVisible = false
-        CreatBtn.textContent = "Explore"
-    } else {
-        try {
-            let box = await fetch( `https://fakestoreapi.com/products/1`)
-            let result = await box.json()
-        
-           
-                let p = document.createElement("p")
-                p.textContent = result.description
-                Creatp.appendChild(p)
-                Creatp.classList.toggle("Fechtex")
-          isVisible= true
-            CreatBtn.textContent = "up"
-            
-           } catch (error) {
-          console.log("error");
-          
-           }
+  if (isVisible) {
+    Creatp.innerHTML = "";
+    isVisible = false;
+    CreatBtn.textContent = "Explore";
+  } else {
+    try {
+      let box = await fetch(`https://fakestoreapi.com/products/1`);
+      let result = await box.json();
+
+      let p = document.createElement("p");
+      p.textContent = result.description;
+      Creatp.appendChild(p);
+      Creatp.classList.toggle("Fechtex");
+      isVisible = true;
+      CreatBtn.textContent = "up";
+    } catch (error) {
+      console.log("error");
     }
-  
-})
+  }
+});
 
-// ეს ღილაკი მუშაობს რომ api და წამოიღოს 3 id ლისთისთვის 
+// ეს ღილაკი მუშაობს რომ api და წამოიღოს 3 id ლისთისთვის
 aboutBtn.addEventListener("click", async (event) => {
   event.preventDefault();
 
   if (isAboutVisible) {
-      ulAdd.innerHTML = "";
-       isAboutVisible  = false;
-      aboutBtn.textContent = "Explore";
+    ulAdd.innerHTML = "";
+    isAboutVisible = false;
+    aboutBtn.textContent = "Explore";
   } else {
-      try {
-          let response = await fetch(`https://fakestoreapi.com/products`);
-          let result = await response.json();
+    try {
+      let response = await fetch(`https://fakestoreapi.com/products`);
+      let result = await response.json();
 
-          result.slice(0, 3).forEach(item => {
-              let li = document.createElement("li");
-              li.textContent = item.title;
-              ulAdd.appendChild(li);
-          });
+      result.slice(0, 3).forEach((item) => {
+        let li = document.createElement("li");
+        li.textContent = item.title;
+        ulAdd.appendChild(li);
+      });
 
-          ulAdd.classList.toggle("li_js");
-          isAboutVisible = true;
-          aboutBtn.textContent = "show less";
-      } catch (error) {
-          console.error("Error fetching data:", error);
-      }
+      ulAdd.classList.toggle("li_js");
+      isAboutVisible = true;
+      aboutBtn.textContent = "show less";
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 });
-
-
 
 burgerIcon.addEventListener("click", () => {
   burgerMenu.classList.toggle("burger_icon");
 });
-
 
 // Email vallidation
 subscribe_button.addEventListener("click", (event) => {
@@ -85,7 +78,6 @@ subscribe_button.addEventListener("click", (event) => {
   let email_value = email.value;
   let email_validation = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   if (email_validation.test(email_value)) {
-    alert("Email is correct");
     email.remove();
   } else {
     alert("Email is not correct");
@@ -107,7 +99,6 @@ browseall.addEventListener("click", () => {
   }
 });
 
-
 // make slider
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -119,4 +110,3 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: true,
   });
 });
-
